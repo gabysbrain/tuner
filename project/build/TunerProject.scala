5ed1f8964249a37f48bd09b2589addd95ec0b8d7
Thomas Torsney-Weir <tom@wakizashi>
2011-04-27 21:04:41 -0700

    depend on scala swing

--- project/build/TunerProject.scala
+++ project/build/TunerProject.scala
@@ -1,6 +1,13 @@
 import sbt._
 
 class TunerProject(info : ProjectInfo) extends DefaultProject(info) {
+
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
+
+  override def compileOptions : Seq[CompileOption] = List(Deprecation)
+
+  override def mainClass : Option[String] = Some("Tuner")
   
 }
 
