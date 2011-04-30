9dac07b8a526c942032b2ab21f350752817d6971
Thomas Torsney-Weir <tom@wakizashi>
2011-04-30 13:00:25 -0700

    add lift-json support

--- project/build/TunerProject.scala
+++ project/build/TunerProject.scala
@@ -5,6 +5,8 @@ class TunerProject(info : ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = ScalaToolsSnapshots
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
 
+  val lift-json = "net.liftweb" % "lift-json" % "2.0"
+
   override def compileOptions : Seq[CompileOption] = List(Deprecation)
 
   override def mainClass : Option[String] = Some("tuner.Tuner")
