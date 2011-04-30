995dadd95e5acd15c0a038cce3b52d5fdb4d1fe3
Thomas Torsney-Weir <tom@wakizashi>
2011-04-30 15:10:41 -0700

    fixed lift specification

--- project/build/TunerProject.scala
+++ project/build/TunerProject.scala
@@ -5,7 +5,7 @@ class TunerProject(info : ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = ScalaToolsSnapshots
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
 
-  val lift-json = "net.liftweb" % "lift-json" % "2.0"
+  val lift_json = "net.liftweb" % "lift-json" % "2.0"
 
   override def compileOptions : Seq[CompileOption] = List(Deprecation)
 
