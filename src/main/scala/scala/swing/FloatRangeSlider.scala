d11fcb4c630ce479fe04391dd6874ae9c549c3bb
Thomas Torsney-Weir <tom@wakizashi>
2011-05-16 12:40:11 -0700

    rename RangeSlider to FloatRangeSlider to bring it in line with the class name

--- src/main/scala/scala/swing/FloatRangeSlider.scala
+++ src/main/scala/scala/swing/FloatRangeSlider.scala
@@ -1,5 +1,7 @@
 package scala.swing
 
+import scala.swing.event.ValueChanged
+
 import prefuse.util.ui.JRangeSlider
 
 class FloatRangeSlider(minVal:Float, maxVal:Float, numSteps:Int) 
@@ -18,5 +20,21 @@ class FloatRangeSlider(minVal:Float, maxVal:Float, numSteps:Int)
     val upperVal = minFloat + step * peer.getHighValue
     (lowerVal, upperVal)
   }
+  def lowValue : Float = value._1
+  def lowValue_=(v:Float) = {
+    val intVal = math.round((v - minFloat)/step)
+    peer.setLowValue(intVal)
+  }
+  def highValue : Float = value._2
+  def highValue_=(v:Float) = {
+    val intVal = math.round((v - minFloat)/step)
+    peer.setHighValue(intVal)
+  }
+
+  peer.addChangeListener(new javax.swing.event.ChangeListener {
+    def stateChanged(e:javax.swing.event.ChangeEvent) {
+      publish(new ValueChanged(FloatRangeSlider.this))
+    }
+  })
 }
 
