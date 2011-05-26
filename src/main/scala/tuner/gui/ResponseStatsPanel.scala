fef313779904c9235ae408ef8c7c66ab0da2f03a
Thomas Torsney-Weir <tom@wakizashi>
2011-05-25 17:03:20 -0700

    hack to get collapsible panel working with processing panels

--- src/main/scala/tuner/gui/ResponseStatsPanel.scala
+++ src/main/scala/tuner/gui/ResponseStatsPanel.scala
@@ -18,6 +18,15 @@ class ResponseStatsPanel(project:Project)
     histogramPanels.foreach {case (fld,panel) =>
       //panels += new CollapsiblePanel.CPanel(fld, panel)
       val p = new org.japura.gui.CollapsiblePanel(fld)
+      p.setAnimationEnabled(false)
+      p.addCollapsiblePanelListener(new org.japura.gui.event.CollapsiblePanelAdapter {
+        override def panelCollapsed(event:org.japura.gui.event.CollapsiblePanelEvent) = {
+          panel.visible = false
+        }
+        override def panelExpanded(event:org.japura.gui.event.CollapsiblePanelEvent) = {
+          panel.visible = true
+        }
+      })
       p.add(panel.peer)
       peer.add(p)
     }
