29cd7ae460dbaebdc56a4b45b3f2dace084115ca
Thomas Torsney-Weir <tom@wakizashi>
2011-05-26 14:19:24 -0700

    the layout is definitely fixed now!

--- src/main/scala/tuner/gui/util/FacetLayout.scala
+++ src/main/scala/tuner/gui/util/FacetLayout.scala
@@ -11,22 +11,20 @@ object FacetLayout {
     val constrainingSize = math.min(bounds.width, bounds.height)
     val spacingSize = Config.plotSpacing * (fields.length - 1)
     val facetSize = (constrainingSize - spacingSize) / fields.length
+    val plotBounds = Rectangle((bounds.minX, bounds.minY),
+                               constrainingSize, constrainingSize)
 
     var plotDims = Map[(String,String),Rectangle]()
-    fields.foldLeft(bounds.minX) {case (xPos, xFld) =>
-      fields.foldLeft(bounds.minY) {case (yPos, yFld) =>
+    fields.foldLeft(plotBounds.minX) {case (xPos, xFld) =>
+      fields.foldLeft(plotBounds.minY) {case (yPos, yFld) =>
         if(xFld < yFld) {
-          /*
-          if(xFld == "minCorrelation" && yFld == "similThreshold")
-            println("y " + yPos + " " + bounds.minY + " " + constrainingSize + " " + bounds.height)
-          */
           val blBounds = 
             Rectangle((xPos, yPos), facetSize, facetSize)
           val trBounds = {
-            val startX = bounds.minX+constrainingSize-xPos-Config.plotSpacing*2
-            val endY = 2*bounds.minY+constrainingSize-yPos
-            val startY = endY - facetSize
-            val endX = startX + facetSize
+            val startY = plotBounds.minY+plotBounds.maxY-(yPos+facetSize)
+            val endX = plotBounds.minX + plotBounds.maxX-xPos
+            val startX = endX - facetSize
+            val endY = startY + facetSize
             Rectangle((startX, startY), (endX, endY))
           }
           plotDims += ((xFld, yFld) -> blBounds)
