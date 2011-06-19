bb13594807dca659d5cc7d500682b48770b7a738
Thomas Torsney-Weir <tom@wakizashi>
2011-06-19 00:01:25 -0700

    make unix sbt also use launch-sbt.jar

--- sbt.sh
+++ sbt.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 export R_HOME=/Library/Frameworks/R.framework/Resources
-sbt $*
+JRI=/Library/Frameworks/R.framework/Versions/Current/Resources/library/rJava/jri
+
+java -Xmx512M -Djava.library.path=${JRI} -jar sbt-launch.jar $*
 
