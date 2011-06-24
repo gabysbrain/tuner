d5e1faebbb0e9112eb4017716ba2b4f564680251
tom <tom@COUNTCRAPULA.localdomain>
2011-06-23 20:37:38 -0700

    no one cares what file you're reading

--- src/main/scala/tuner/util/FileReader.scala
+++ src/main/scala/tuner/util/FileReader.scala
@@ -5,7 +5,6 @@ import scala.io.Source
 object FileReader {
 
   def read(path:String) = {
-    println("reading " + path)
     val lines = Source.fromFile(path).getLines
     //Source.fromFile(path).getLines.filter {ln => ln != ""} toIterator
     lines.filter {ln => ln != ""}
