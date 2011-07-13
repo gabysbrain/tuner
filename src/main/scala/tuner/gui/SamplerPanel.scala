032b095f5f22328aaca3d812e3d84ed5a43314cc
Thomas Torsney-Weir <tom@wakizashi>
2011-07-13 13:11:33 -0700

    sampler panel is handling input correctly but generating too many updates

--- src/main/scala/tuner/gui/SamplerPanel.scala
+++ src/main/scala/tuner/gui/SamplerPanel.scala
@@ -1,5 +1,7 @@
 package tuner.gui
 
+import date.scala.swing.TimeField
+
 import scala.swing.BorderPanel
 import scala.swing.BoxPanel
 import scala.swing.ComboBox
@@ -18,25 +20,25 @@ class SamplerPanel(project:tuner.project.Sampler)
     extends BoxPanel(Orientation.Vertical) {
   
   val sampleNumField = new TextField
-  val sampleTimeField = new TextField
-  val ttlRunTimeField = new TextField
+  val sampleTimeField = new TimeField
+  val ttlRunTimeField = new TimeField
   val shapeSelector = new RegionShapeCombo
   val methodSelector = new ComboBox(List("LHS", "Random"))
 
-  val controlPane = new TablePanel(2, 2) {
+  val controlPane = new TablePanel(2, 4) {
     // Labels in left column
     layout(new Label("Number of Samples")) = (0,0)
-    //layout(new Label("x Time per Sample")) = (0,1)
-    //layout(new Label("= Total Run Time")) = (0,2)
+    layout(new Label("x Time per Sample")) = (0,1)
+    layout(new Label("= Total Run Time")) = (0,2)
     //layout(new Label("Shape")) = (0,3)
-    layout(new Label("Method")) = (0,1)
+    layout(new Label("Method")) = (0,3)
 
     // Fields in left column
     layout(sampleNumField) = (1,0)
-    //layout(sampleTimeField) = (1,1)
-    //layout(ttlRunTimeField) = (1,2)
+    layout(sampleTimeField) = (1,1)
+    layout(ttlRunTimeField) = (1,2)
     //layout(shapeSelector) = (1,3)
-    layout(methodSelector) = (1,1)
+    layout(methodSelector) = (1,3)
 
     border = Swing.TitledBorder(border, "Sampling")
   }
@@ -48,19 +50,54 @@ class SamplerPanel(project:tuner.project.Sampler)
 
   // Set up the events
   listenTo(sampleNumField)
-  //listenTo(shapeSelector)
+  listenTo(sampleTimeField)
+  listenTo(ttlRunTimeField)
+  //listenTo(shapeSelector.selection)
   listenTo(methodSelector.selection)
 
   var lastSamples:Int = numSamples
   var lastSelection:String = methodString
 
+  // Keep track of all the text field update times
+  var sampleNumModTime:Long = System.currentTimeMillis
+  var sampleTimeModTime:Long = System.currentTimeMillis
+  var ttlTimeModTime:Long = System.currentTimeMillis
+
+  var internalChange:Boolean = false
+
   reactions += {
-    case ValueChanged(`sampleNumField`) | SelectionChanged(`methodSelector`) => 
-      // Only publish if something actually changed
-      if(lastSamples != numSamples || lastSelection != methodString) {
-        lastSamples = numSamples
-        lastSelection = methodString
-        publish(new ValueChanged(this))
+    case SelectionChanged(`methodSelector`) => 
+      handleSamplesChanged
+    case ValueChanged(`sampleNumField`) =>
+      handleSamplesChanged
+      if(!internalChange) {
+        sampleNumModTime = System.currentTimeMillis
+        internalChange = true
+        if(sampleTimeModTime > ttlTimeModTime)
+          updateTotalTime
+        else
+          updateSampleTime
+        internalChange = false
+      }
+    case ValueChanged(`sampleTimeField`) =>
+      if(!internalChange) {
+        sampleTimeModTime = System.currentTimeMillis
+        internalChange = true
+        if(sampleNumModTime > ttlTimeModTime)
+        updateTotalTime
+          else
+          updateNumSamples
+        internalChange = false
+      }
+    case ValueChanged(`ttlRunTimeField`) => 
+      if(!internalChange) {
+        ttlTimeModTime = System.currentTimeMillis
+        internalChange = true
+        if(sampleNumModTime > sampleTimeModTime)
+          updateSampleTime
+        else
+          updateNumSamples
+        internalChange = false
       }
   }
 
@@ -83,5 +120,37 @@ class SamplerPanel(project:tuner.project.Sampler)
       case "Random" => Sampler.random
     }
   }
+
+  private def handleSamplesChanged = {
+    // Only publish if something actually changed
+    if(lastSamples != numSamples || lastSelection != methodString) {
+      lastSamples = numSamples
+      lastSelection = methodString
+      publish(new ValueChanged(this))
+    }
+  }
+
+  private def updateNumSamples = {
+    (sampleTimeField.millis, ttlRunTimeField.millis) match {
+      case (Some(st), Some(tt)) if(st > 0) => 
+        sampleNumField.text = (tt/st).toInt.toString
+      case _ =>
+    }
+  }
+
+  private def updateSampleTime = {
+    (ttlRunTimeField.millis, numSamples > 0) match {
+      case (Some(tt), true) => sampleTimeField.millis = tt / numSamples
+      case _ =>
+    }
+  }
+
+  private def updateTotalTime = {
+    (sampleTimeField.millis, numSamples > 0) match {
+      case (Some(st), true) => ttlRunTimeField.millis = numSamples * st
+      case _ =>
+    }
+  }
+
 }
 
