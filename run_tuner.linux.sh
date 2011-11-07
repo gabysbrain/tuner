f28e02fe74711b852a85f6642f41a1c3520c5c42
Thomas Torsney-Weir <tom@tanto>
2011-11-06 22:45:40 -0800

    better link to R modules

--- run_tuner.linux.sh
+++ run_tuner.linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JRI=/home/tom/R/x86_64-redhat-linux-gnu-library/2.13/rJava/jri
+JRI=${HOME}/R/x86_64-redhat-linux-gnu-library/2.13/rJava/jri
 export R_HOME=/usr/lib64/R
 OPENGL=lib/opengl/linux64
 
