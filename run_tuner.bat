f2f596e694669096df1e3da51f7f6e2c2e9923c1
Thomas Torsney-Weir <tom@wakizashi>
2011-11-07 10:00:43 -0800

    more universal run_tuner.bat

--- run_tuner.bat
+++ run_tuner.bat
@@ -1,9 +1,8 @@
 
-REM set R_HOME=\\freesia\userdata$\ttorsney\Documents\R\R-2.13.0
-set R_HOME=C:\Program Files\R\R-2.13.0
-set R_PKG=C:\Users\tom\Documents\R\win-library\2.13
+set R_HOME=C:\Program Files\R\R-2.14.0
+set R_PKG=%HOMEPATH%\Documents\R\win-library\2.14
 set JRI=%R_PKG%\rJava\jri\x64;%R_PKG%\rJava\jri
-set OPENGL=lib\windows64
+set OPENGL=lib\opengl\windows64
 
 set PATH=%PATH%;%R_HOME%\bin;%R_HOME%\bin\x64;%OPENGL%
 
