c6b9be5735f17ae6d409ef124c36aa6a68d6fb84
Thomas Torsney-Weir <tom@wakizashi>
2012-02-03 18:56:12 -0800

    forgot to add error shader

---
+++ src/main/resources/shaders/err.plot.frag.glsl
@@ -0,0 +1,21 @@
+
+#version 120
+
+varying float fragCoeff;
+varying float centerSqDist;
+varying vec2 vertexDist;
+varying vec2 theta;
+
+uniform float sig2;
+
+void main() {
+  vec2 vertexSqDist = vertexDist * vertexDist;
+  vec2 weightedDist = theta * vertexSqDist;
+  float ttlDist = centerSqDist + weightedDist.x + weightedDist.y;
+  float tmp = exp(-ttlDist);
+  float redness = sig2 * fragCoeff * tmp * tmp;
+  //alpha = 0.4;
+  //alpha = 1.0;
+  gl_FragColor = vec4(redness, 0.0, 0.0, 1.0);
+}
+
