3902bb3b3b4a61889836487ff0129bcf8285f7f4
Thomas Torsney-Weir <torsneyt@gmail.com>
2012-08-23 15:48:54 -0700

    fix path to Tuner-assembly-0.9.jar

--- run_tuner.mac.sh
+++ run_tuner.mac.sh
@@ -4,5 +4,5 @@ JRI=/Library/Frameworks/R.framework/Versions/Current/Resources/library/rJava/jri
 export R_HOME=/Library/Frameworks/R.framework/Resources
 OPENGL=lib/macosx
 
-java -Djava.library.path=${JRI}:${OPENGL} -jar Tuner-assembly-0.9.jar
+java -Djava.library.path=${JRI}:${OPENGL} -jar target/Tuner-assembly-0.9.jar
 
