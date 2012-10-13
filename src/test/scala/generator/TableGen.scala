008e34dcb3b677d48f427f14249e265fe1a134a5
Thomas Torsney-Weir <torsneyt@gmail.com>
2012-10-12 20:27:09 -0700

    2d data table is too restrictive for GP testing

--- src/test/scala/generator/TableGen.scala
+++ src/test/scala/generator/TableGen.scala
@@ -6,10 +6,10 @@ import tuner.Table
 
 object TableGen {
   
-  def tableType : Gen[Table] = Gen.sized {rows => for {
-    d <- Gen.choose(2, 10)
+  def tableType(size:Int) : Gen[Table] = for {
+    d <- Gen.choose(3, 10)
     fields <- Util.fieldListType(d)
-    vals <- Gen.listOfN(rows, Gen.listOfN(d, Gen.choose(0f,1f)))
+    vals <- Gen.listOfN(size, Gen.listOfN(d, Gen.choose(0f,1f)))
   } yield {
     val tbl = new Table
     vals.foreach {row =>
