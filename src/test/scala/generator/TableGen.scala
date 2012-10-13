ad492e0c3d61e78da56d299274c8bfbd77663389
Thomas Torsney-Weir <torsneyt@gmail.com>
2012-10-12 20:27:41 -0700

    create table generator for tables with more columns than rows

--- src/test/scala/generator/TableGen.scala
+++ src/test/scala/generator/TableGen.scala
@@ -16,6 +16,22 @@ object TableGen {
       tbl.addRow(fields.zip(row))
     }
     tbl
-  }}
+  }
+
+  def wideTableType(size:Int) : Gen[Table] = for {
+    d <- Gen.choose(size, size*2)
+    fields <- Util.fieldListType(d)
+    vals <- Gen.listOfN(size, Gen.listOfN(d, Gen.choose(0f,1f)))
+  } yield {
+    val tbl = new Table
+    vals.foreach {row =>
+      tbl.addRow(fields.zip(row))
+    }
+    tbl
+  }
+
+
+  val tableGen = Gen.sized {size => tableType(size)}
+  val wideTableGen = Gen.sized {size => wideTableType(size)}
 }
 
