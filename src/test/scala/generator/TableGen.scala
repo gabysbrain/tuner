49c8e285d27dca07d9d17366446e06a4c004378a
Thomas Torsney-Weir <torsneyt@gmail.com>
2012-10-13 21:50:34 -0700

    create tables with fixed field names

--- src/test/scala/generator/TableGen.scala
+++ src/test/scala/generator/TableGen.scala
@@ -6,30 +6,38 @@ import tuner.Table
 
 object TableGen {
   
-  def tableType(size:Int) : Gen[Table] = for {
-    d <- Gen.choose(3, 10)
-    fields <- Util.fieldListType(d)
-    vals <- Gen.listOfN(size, Gen.listOfN(d, Gen.choose(0f,1f)))
+  def tableType(rows:Int, fields:List[String]) = for {
+    vals <- Gen.listOfN(rows, Gen.listOfN(fields.length, Gen.choose(0f,1f)))
   } yield {
     val tbl = new Table
     vals.foreach {row =>
       tbl.addRow(fields.zip(row))
     }
+    tbl
+  }
+
+  def tableType(size:Int) : Gen[Table] = for {
+    d <- Gen.choose(3, 10)
+    fields <- Util.fieldListType(d)
+    tbl <- tableType(size, fields)
+  } yield {
     tbl
   }
 
   def wideTableType(size:Int) : Gen[Table] = for {
     d <- Gen.choose(size, size*2)
     fields <- Util.fieldListType(d)
-    vals <- Gen.listOfN(size, Gen.listOfN(d, Gen.choose(0f,1f)))
+    tbl <- tableType(size, fields)
   } yield {
-    val tbl = new Table
-    vals.foreach {row =>
-      tbl.addRow(fields.zip(row))
-    }
     tbl
   }
 
+  def trainTestTableGen : Gen[(Table,Table)] = Gen.sized {size => for {
+    trainTbl <- tableType(size)
+    testTbl <- tableType(size, trainTbl.fieldNames)
+  } yield {
+    (trainTbl, testTbl)
+  }}
 
   val tableGen = Gen.sized {size => tableType(size)}
   val wideTableGen = Gen.sized {size => wideTableType(size)}
