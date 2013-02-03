6f12856de5b00eebbb2759aedb2a4a8dda7eb481
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-02-03 16:05:07 +0100

    fix mac paths on start script

--- run_tuner.mac.sh
+++ run_tuner.mac.sh
@@ -2,7 +2,7 @@
 
 JRI=/Library/Frameworks/R.framework/Versions/Current/Resources/library/rJava/jri
 export R_HOME=/Library/Frameworks/R.framework/Resources
-OPENGL=lib/macosx
+OPENGL=lib/opengl/macosx
 
-java -Djava.library.path=${JRI}:${OPENGL} -jar target/Tuner-assembly-0.2.jar
+java -Djava.library.path=${JRI}:${OPENGL} -jar Tuner-assembly-0.2.jar
 
