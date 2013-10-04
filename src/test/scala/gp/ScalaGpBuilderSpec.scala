9f6cf6fa9f95b10c4120c76871742eb11209c727
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-10-04 14:27:08 +0200

    more logical placement of test code

--- src/test/scala/gp/ScalaGpBuilderSpec.scala
+++ src/test/scala/gp/ScalaGpBuilderSpec.scala
@@ -10,20 +10,23 @@ class ScalaGpBuilderSpec extends WordSpec {
 
   "A ScalaGpBuilder class" when {
     "given a 3D test data set" should {
+      val dataset = resource("/datasets/3d.csv")
+      val params = List("x1", "x2", "x3")
+      val resp = "y1"
+
       "return a valid model" in {
-        val gp = ScalaGpBuilder.buildModel(resource("/datasets/3d.csv"), 
-                                           List("x1", "x2", "x3"), 
-                                           "y1", "sd")
+        val gp = ScalaGpBuilder.buildModel(dataset, params, resp, "sd")
         assert(gp.validateModel._1)
       }
     }
 
     "given an 8D test data set" should {
+      val dataset = resource("/datasets/8d.csv")
+      val params = List("x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8")
+      val resp = "y6"
+
       "return a valid model" in {
-        val gp = ScalaGpBuilder.buildModel(
-          resource("/datasets/8d.csv"), 
-          List("x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8"), 
-          "y6", "sd")
+        val gp = ScalaGpBuilder.buildModel(dataset, params, resp, "sd")
         assert(gp.validateModel._1)
       }
     }
