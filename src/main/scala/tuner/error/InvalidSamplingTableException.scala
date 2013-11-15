0626e849cb6d5b2ba147edfb7049ec5d1ca70015
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-11-15 17:09:44 +0100

    actually produce an error message

--- src/main/scala/tuner/error/InvalidSamplingTableException.scala
+++ src/main/scala/tuner/error/InvalidSamplingTableException.scala
@@ -3,5 +3,13 @@ package tuner.error
 import tuner.Table
 
 class InvalidSamplingTableException(val input:Table, val output:Table)
-  extends Exception
+  extends Exception(
+    if(!input.fieldNames.toSet.subsetOf(output.fieldNames.toSet)) {
+      val inputFields = input.fieldNames.mkString(", ")
+      val outputFields = output.fieldNames.mkString(", ")
+      s"Input table fields ($inputFields) does not match output table fields ($outputFields)"
+    } else {
+      "Input table does not match output table"
+    }
+  )
 
