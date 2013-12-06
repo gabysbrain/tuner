1b6a15456091d871938ca803072745270550ffca
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-12-06 09:57:12 +0100

    switch ScalaGpBuilder to should matchers

--- src/test/scala/tuner/integration/ScalaGpBuilderSpec.scala
+++ src/test/scala/tuner/integration/ScalaGpBuilderSpec.scala
@@ -1,6 +1,7 @@
 package tuner.test.integration
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
 import tuner.gp.ScalaGpBuilder
 
@@ -9,25 +10,25 @@ import tuner.test.Util._
 class ScalaGpBuilderSpec extends WordSpec {
 
   "A ScalaGpBuilder class" when {
-    "given a 3D test data set" should {
+    "given a 3D test data set" must {
       val dataset = resource("/datasets/3d.csv")
       val params = List("x1", "x2", "x3")
       val resp = "y1"
 
       "return a valid model" in {
         val gp = ScalaGpBuilder.buildModel(dataset, params, resp, "sd")
-        assert(gp.validateModel._1)
+        gp.validateModel._1 should be (true)
       }
     }
 
-    "given an 8D test data set" should {
+    "given an 8D test data set" must {
       val dataset = resource("/datasets/8d.csv")
       val params = List("x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8")
       val resp = "y6"
 
       "return a valid model" in {
         val gp = ScalaGpBuilder.buildModel(dataset, params, resp, "sd")
-        assert(gp.validateModel._1)
+        gp.validateModel._1 should be (true)
       }
     }
   }
