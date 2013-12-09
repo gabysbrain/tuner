3798379f59f8980b9740b8e4ef03da482b7b9225
Hans Meine <hans_meine@gmx.net>
2013-12-09 19:24:28 +0100

    fix linux build script
    The Tuner-assembly .jar is in a different position, and
    the data-scala cloning can be skipped if the directory is present.

--- build-tuner-linux.sh
+++ build-tuner-linux.sh
@@ -5,9 +5,10 @@ JRI="${HOME}${R_LIBS_USER#\~}/rJava/jri"
 R_HOME=`R RHOME`
 export R_HOME
 
+test -d date-scala || \
 git clone https://bitbucket.org/gabysbrain/date.scala.git date-scala
 
 java -Xmx512M "-Djava.library.path=$JRI" -jar sbt-launch.jar assembly \
-	&& mv target/Tuner-assembly-0.9.jar  .
+	&& mv target/scala*/Tuner-assembly-*.jar  .
 
 
