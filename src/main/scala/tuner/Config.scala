b618e7c037bc3e8b2daa7c2f49a555a1329eb275
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-01-13 15:59:42 +0100

    use travis' headless mode so awt will load

--- src/main/scala/tuner/Config.scala
+++ src/main/scala/tuner/Config.scala
@@ -1,6 +1,6 @@
 package tuner
 
-//import java.awt.Toolkit
+import java.awt.Toolkit
 
 import java.io.File
 import java.io.FileInputStream
@@ -18,8 +18,7 @@ object Config {
   val respSampleFilename = "response_samples.csv"
   val imageDirname = "images"
 
-  //val smallScreen = Toolkit.getDefaultToolkit.getScreenSize.height < 800
-  val smallScreen = false
+  val smallScreen = Toolkit.getDefaultToolkit.getScreenSize.height < 800
 
   val errorField = "stddev"
   val gainField = "estgain"
