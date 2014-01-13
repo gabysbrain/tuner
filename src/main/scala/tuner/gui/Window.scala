b9119e5a1aae399b75038f795b0874c07a1f500c
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-01-13 10:07:44 +0100

    cleanup

--- src/main/scala/tuner/gui/Window.scala
+++ src/main/scala/tuner/gui/Window.scala
@@ -11,18 +11,6 @@ abstract class Window(val project:Project) extends Frame {
   // register this window with Tuner
   Tuner.listenTo(this)
 
-  /*
-  override def closeOperation = {
-    Tuner.deafTo(this)
-
-    menuBar match {
-      case mb:tuner.gui.MainMenu => WindowMenu.menus -= mb
-      case _ =>
-    }
-    dispose
-  }
-  */
-
   def toFront = {
     visible = true
     peer.toFront
