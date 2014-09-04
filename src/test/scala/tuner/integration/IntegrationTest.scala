7dc59b0930e9ab353987f065592d22b2eac595d2
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-09-04 09:26:07 +0200

    forgot to include base test classes!!!

---
+++ src/test/scala/tuner/integration/IntegrationTest.scala
@@ -0,0 +1,7 @@
+package tuner.test.integration
+
+import org.specs2.specification.AllExpectations
+import org.specs2.mutable._
+
+class IntegrationTest extends SpecificationWithJUnit with AllExpectations {
+}
---
+++ src/test/scala/tuner/unit/UnitTest.scala
@@ -0,0 +1,10 @@
+package tuner.test.unit
+
+import org.specs2.specification.AllExpectations
+import org.specs2.mutable._
+
+class UnitTest extends SpecificationWithJUnit with AllExpectations {
+
+
+}
+
