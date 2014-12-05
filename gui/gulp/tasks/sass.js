4a3534c46e578d1b69903da571db609dfac9c2d3
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-12-05 10:38:37 +0100

    upgrade to foundation for apps which requires ruby-sass for now...

--- gui/gulp/tasks/sass.js
+++ gui/gulp/tasks/sass.js
@@ -2,10 +2,10 @@
 
 var config = require('../config');
 var gulp = require('gulp');
-// var prefix = require('gulp-autoprefixer');
+var prefix = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
 var fingerprint = require('gulp-fingerprint');
-var sass = require('gulp-sass');
+var sass = require('gulp-ruby-sass');
 var size = require('gulp-size');
 
 // Styles
@@ -13,11 +13,12 @@ gulp.task('styles', function () {
   // See https://github.com/andrew/node-sass for more options
   return gulp.src('app/scss/app.scss')
     .pipe(sass({
-      outputStyle: 'nested',
-      includePaths: [config.bower + '/foundation/scss'],
-      sourceComments: 'map'
+      style: 'nested',
+      loadPath: [config.bower + '/foundation-apps/scss']
     }))
-    // .pipe(prefix('last 1 version'))  // add vendor prefixes if necessary
+    .pipe(prefix({
+      browsers: ['last 2 versions', 'ie 10']
+    }))
     .pipe(gulp.dest(config.dist + '/styles'))
     .pipe(size());
 });
