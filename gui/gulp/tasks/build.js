e4b41be9cf76db751e761c6cec4e9f642ca0f43b
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-12-16 18:19:19 +0100

    remove gulp dependency

--- gui/gulp/tasks/build.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var rev = require('gulp-rev');
-
-// Build
-gulp.task('build', ['html', 'styles', 'vendor', 'browserify', 'images'], function() {
-
-});
--- gui/gulp/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var rimraf = require('gulp-rimraf');
-
-// Clean
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'dist/styles', 'dist/scripts', 'dist/images'], {read: false}).pipe(rimraf());
-});
