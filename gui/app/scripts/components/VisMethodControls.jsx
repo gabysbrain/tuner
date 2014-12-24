9714533dc9bccc37ea54519deaa1240f41836f3f
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-12-24 13:04:26 +0100

    vis method controls

---
+++ gui/app/scripts/components/VisMethodControls.jsx
@@ -0,0 +1,44 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var VisMethodControls = React.createClass({
+
+  propTypes: {
+    value: PropTypes.string.isRequired,
+    actions: PropTypes.object.isRequired
+  },
+
+  changeMethod: function(e) {
+    this.props.actions.changePSView(e.target.value);
+  },
+
+  render: function() {
+    return (
+      <ul className="controls">
+        <li>
+          <label for="hyperslice-vis">
+            Hyperslice
+            <input type="radio" id="hyperslice-vis" name="vis-method"
+                   value="hyperslice"
+                   checked={this.props.value==="hyperslice"}
+                   onChange={this.changeMethod} />
+          </label>
+        </li>
+        <li>
+          <label for="splom-vis">
+            SPLOM
+            <input type="radio" id="splom-vis" name="vis-method"
+                   value="splom"
+                   checked={this.props.value==="splom"}
+                   onChange={this.changeMethod} />
+          </label>
+        </li>
+      </ul>
+    );
+  }
+
+});
+
+module.exports = VisMethodControls;
---
+++ gui/app/scripts/components/__tests__/VisMethodControls-test.jsx
@@ -0,0 +1,63 @@
+/** @jsx React.DOM */
+
+require('./react_helper');
+
+var Controls, actions;
+
+describe('Vis method controls', function() {
+
+  beforeEach(function() {
+    Controls = require('../VisMethodControls.jsx');
+    actions = {
+      changePSView: sinon.spy()
+    };
+  });
+
+  it('has all the methods', function() {
+    var ctrl = TestUtils.renderIntoDocument(
+      <Controls value="hyperslice"
+                actions={actions} />
+    );
+
+    var inputs = TestUtils.scryRenderedDOMComponentsWithTag(ctrl, 'input');
+    expect(inputs.length).to.equal(2);
+  });
+
+  it('has the correct method selected', function() {
+    var ctrl = TestUtils.renderIntoDocument(
+      <Controls value="hyperslice"
+                actions={actions} />
+    );
+
+    var inputs = TestUtils.scryRenderedDOMComponentsWithTag(ctrl, 'input');
+    expect(inputs[0].getDOMNode().hasAttribute('checked')).to.be.true;
+    expect(inputs[1].getDOMNode().hasAttribute('checked')).to.be.false;
+  });
+
+  it('changes to the hyperslice view', function() {
+    var ctrl = TestUtils.renderIntoDocument(
+      <Controls value="splom"
+                actions={actions} />
+    );
+
+    var inputs = TestUtils.scryRenderedDOMComponentsWithTag(ctrl, 'input');
+    TestUtils.Simulate.change(inputs[0]);
+
+    expect(actions.changePSView.callCount).to.equal(1);
+    expect(actions.changePSView.getCall(0).args[0]).to.equal('hyperslice');
+  });
+
+  it('changes to the splom view', function() {
+    var ctrl = TestUtils.renderIntoDocument(
+      <Controls value="hyperslice"
+                actions={actions} />
+    );
+
+    var inputs = TestUtils.scryRenderedDOMComponentsWithTag(ctrl, 'input');
+    TestUtils.Simulate.change(inputs[1]);
+
+    expect(actions.changePSView.callCount).to.equal(1);
+    expect(actions.changePSView.getCall(0).args[0]).to.equal('splom');
+  });
+
+});
