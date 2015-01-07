1074262ba93ab828bf86560e2f1d1d9429691ae4
Thomas Torsney-Weir <torsneyt@gmail.com>
2015-01-07 12:33:31 +0100

    watch css changes for browser-sync

--- gui/gulp/tasks/css.js
+++ gui/gulp/tasks/css.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var filter = require('gulp-filter');
 var config = require('../config');
 
 gulp.task('sass', function() {
+  var browserSync = require('browser-sync').reload;
   return gulp.src(config.src.scss + '/app.scss')
     .pipe(sass())
     .on('error', function(err) {console.error(err.message)})
-    .pipe(gulp.dest(config.dest.css));
+    .pipe(gulp.dest(config.dest.css))
+    .pipe(filter('**/*.css')) // only send css files to browsersync
+    .pipe(browserSync({stream: true}));
 });
