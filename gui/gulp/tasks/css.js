8d02a8c87f3b5634da69efd8fa8b3cc0d24467e5
Thomas Torsney-Weir <torsneyt@gmail.com>
2015-03-18 14:20:27 +0100

    update ruby-sass

--- gui/gulp/tasks/css.js
+++ gui/gulp/tasks/css.js
@@ -5,8 +5,7 @@ var config = require('../config');
 
 gulp.task('sass', function() {
   var browserSync = require('browser-sync').reload;
-  return gulp.src(config.src.scss + '/app.scss')
-    .pipe(sass())
+  return sass(config.src.scss + '/app.scss', {sourcemap: true})
     .on('error', function(err) {console.error(err.message)})
     .pipe(gulp.dest(config.dest.css))
     .pipe(filter('**/*.css')) // only send css files to browsersync
