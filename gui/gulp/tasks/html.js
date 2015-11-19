6c7bdcf0e1190a76b5bc0a3b317088de18ff823e
Thomas Torsney-Weir <torsneyt@gmail.com>
2015-11-19 05:47:18 +0100

    remove gulp and fix package versions

--- gui/gulp/tasks/html.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config');
-
-gulp.task('html', function() {
-  return gulp.src(config.src.base + '/index.html')
-    .pipe(gulp.dest(config.dest.base));
-})
--- gui/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-
-requireDir('./gulp/tasks');
-
-gulp.task('default', ['build'], function() {
-  gulp.run('serve');
-  gulp.start('watch');
-})
